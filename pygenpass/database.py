fd3db01db190ba2fdc6075be961c92bcf6f25a10
chitalekunal <chitalekunal@hotmail.com>
2020-01-27 22:23:37 +0530

    Added by chitalekunal

--- pygenpass/database.py
+++ pygenpass/database.py
@@ -26,32 +26,77 @@ import sqlite3  # library for database
 class DatabaseConnection:
     """ Class of database entries for user's information."""
 
-    def __init__(self):
+    def __create_table__(self,table_schema):
+        """
+        Create table on runtime based on the values that are passed in structure in below dict() format
+        {
+            "tablename":"Value",
+            "field_name":{
+                "datatype":<NULL/INTEGER/REAL/TEXT/BLOB>
+                "nullablity":true/false,
+                "primary_key":true/false,
+                "unique_key":true/false,
+            },
+        }
+        """
+        structure=" "
+        for key in table_schema:
+            if key != "tablename":
+                structure=structure+" "+key+" "
+                for key2 in table_schema[key]:
+                    if key2 == "nullablity" and table_schema[key2] == "false":
+                        structure=structure+" NOT NULL "
+                    elif key2 == "primary_key" and table_schema[key2] == "true":
+                        structure=structure+" PRIMARY KEY "
+                    elif key2 == "unique_key" and table_schema[key2] == "true":
+                        structure=structure+" UNIQUE "
+                
+                structure=structure+" "+table_schema["datatype"]+","
+        self.cursor_obj.execute(structure)
+
+
+    def __init__(self,table_schema=None):
         """Used to create database and then to connect with generated databse file
         Checked for table is created? if not then created as per required values """
         self.con = sqlite3.connect("generated_password.db")
         self.cursor_obj = self.con.cursor()
-        self.cursor_obj.execute(
-            """CREATE TABLE IF NOT EXISTS passwords
-    		  (id integer PRIMARY KEY,portal_name text NOT NULL UNIQUE, password varchar,
-    		  creation_date varchar, email varchar, portal_url varchar)
-    		"""
-        )
+        if (table_schema != None):
+            __create_table__(table_schema)
+        else:
+            self.cursor_obj.execute(
+                """CREATE TABLE IF NOT EXISTS passwords
+    		    (id integer PRIMARY KEY,portal_name text NOT NULL UNIQUE, password varchar,
+    		    creation_date varchar, email varchar, portal_url varchar)
+    		    """
+            )
         self.con.commit()
 
-    def insert_data(self, portal_name, password, creation_date, email, portal_url):
-        """Adding values into database"""
-        self.password = password
-        self.creation_date = creation_date
-        self.email = email
-        self.portal_name = portal_name
-        self.portal_url = portal_url
-        self.cursor_obj.execute(
-            """INSERT INTO passwords
-            (portal_name, password, creation_date, email, portal_url)
-            VALUES (?, ?, ?, ?, ?)""",
-            (self.portal_name, self.password, self.creation_date, self.email, self.portal_url,),
-        )
+    def insert_data(self, portal_name=None, password=None, creation_date=None, email=None, portal_url=None,insert_record=None):
+        """
+        Adding values into database
+        {
+            "tablename":"table_name",
+            "field1":value,
+            .
+            .
+            .
+        }
+        """
+        if insert_record != None:
+            self.password = password
+            self.creation_date = creation_date
+            self.email = email
+            self.portal_name = portal_name
+            self.portal_url = portal_url
+            self.cursor_obj.execute(
+                """INSERT INTO passwords
+                (portal_name, password, creation_date, email, portal_url)
+                VALUES (?, ?, ?, ?, ?)""",
+                (self.portal_name, self.password, self.creation_date, self.email, self.portal_url,),
+            )
+        else:
+            structure="INSERT INTO "+insert_record["tablename"]+"("+','.join(key for key in insert_record if insert_record!= "tablename"])+") VALUES ("+','.join(insert_key[key] for key in insert_record if insert_record!= "tablename"])+")"
+            self.cursor_obj.execute(structure)
         self.con.commit()
 
     def delete_data(self, portal_name):
